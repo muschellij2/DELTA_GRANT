6ed02b2096bc969dc11bc82d6d1de9c0c2b96ffc
muschellij2 <muschellij2@gmail.com>
2018-03-30 18:21:06 -0400

    fixing

--- quickplot.R
+++ quickplot.R
@@ -1,11 +1,11 @@
 library(dplyr)
 library(ggplot2)
-df = data_frame(x = rnorm(100)) %>% 
+df = data_frame(x = rnorm(100)) %>%
   mutate(y = 0.2* x + x^2 * 0.5 + rnorm(100))
-g = df %>% 
+g = df %>%
   ggplot(aes(x, y)) +
-  geom_point() + 
+  geom_point() +
   geom_smooth(se = FALSE)
-png("quickplot.png")
+png("quickplot.png", height = 7, width = 14, units = "in", res = 300)
 g
 dev.off()
