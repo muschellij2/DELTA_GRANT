e1130b6a8493f88509577e3ccef5f542a456188b
muschellij2 <muschellij2@gmail.com>
2018-04-03 17:01:04 -0400

    fixing things

--- auto/render_video.R
+++ auto/render_video.R
@@ -14,22 +14,25 @@ ari_spin(paragraphs = para,
          images = files, output = "joey.mp4",
          voice = "Joey",
          ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
-ari_spin(paragraphs = para,
-         images = files, output = "kim.mp4",
-         voice = "Kimberly",
-         ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
 
-# fname = "script_es.txt"
-# if (file.exists(fname)) {
-#   para = readLines(fname)
-#   para = para[ !para %in% ""]
-#
-#   ari_spin(paragraphs = para,
-#            images = files, output = "miguel.mp4",
-#            voice = "Miguel",
-#            ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
-#   ari_spin(paragraphs = para,
-#            images = files, output = "penelope.mp4",
-#            voice = "Penelope",
-#            ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
-# }
+
+# ari_spin(paragraphs = para,
+#          images = files, output = "kim.mp4",
+#          voice = "Kimberly",
+#          ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
+
+fname = "script_es.txt"
+
+if (file.exists(fname)) {
+  para = readLines(fname)
+  para = para[ !para %in% ""]
+
+  # ari_spin(paragraphs = para,
+  #          images = files, output = "miguel.mp4",
+  #          voice = "Miguel",
+  #          ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
+  ari_spin(paragraphs = para,
+           images = files, output = "penelope.mp4",
+           voice = "Penelope",
+           ffmpeg_opts = '-vf "scale=trunc(iw/2)*2:trunc(ih/2)*2"')
+}
