3a1974b944ef38837b832c4927cd3ecdafed38aa
muschellij2 <muschellij2@gmail.com>
2018-04-03 15:12:06 -0400

    added all the things

--- auto/get_notes.R
+++ auto/get_notes.R
@@ -1,20 +1,37 @@
 library(xml2)
 library(magrittr)
-library(officer)
 library(here)
+library(utf8)
 
-x = read_pptx(file.path(here::here(), "auto", "DELTA2.pptx"))
-notes_dir = file.path(x$package_dir, "ppt", "notesSlides")
-files = list.files(pattern = ".xml$",
-                   path = notes_dir, full.names = TRUE)
-get_notes = function(fname) {
-  xdoc = read_xml(fname)
+fname = file.path(here::here(), "auto", "DELTA2.pptx")
+
+xml_notes = function(xml_fname) {
+  require(xml2)
+  require(magrittr)
+  xdoc = read_xml(xml_fname)
   txt = xdoc %>% xml_find_all("//a:t") %>% xml_text()
   txt = paste(txt, collapse = " ")
-  cat(txt)
-  return(txt)
 }
-script = sapply(files, get_notes)
+get_gs_pptx_notes = function(fname) {
+  require(utf8)
+  tdir = tempfile()
+  dir.create(tdir)
+  res = unzip(fname, exdir = tdir)
+  note_dir = file.path(tdir, "ppt", "notesSlides")
+  notes = list.files(path = note_dir, pattern = "[.]xml$",
+                     full.names = TRUE)
+  if (length(notes) > 0) {
+    res = sapply(notes, xml_notes)
+    names(res) = basename(notes)
+    res = utf8::utf8_normalize(res, map_quote = TRUE)
+    return(res)
+  } else {
+    return(NULL)
+  }
+}
+
+
+script = get_gs_pptx_notes(fname)
 
 writeLines(script, con = "script.txt")
 
